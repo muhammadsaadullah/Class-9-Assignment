e6398276e7296cb1e890fe58f8c330cd3dafb686
[Muahmmad Saadullah] <msaadullah2022@gmail.com>
2024-11-15 23:58:06 +0500

    vercel req changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import HeroSection from "./components/HeroSection";
 import HeroSection2 from "./components/HeroSection2";
 
 export default function Home() {
   return (  
-    <div>
+    <div className="flex flex-col gap-5">
+      <HeroSection />
       <HeroSection2 />
     </div>
   );
